6c010e9209178b447f2f40f63a52447193c23ca4
of-sczhou <abonent@hotmail.com>
2026-01-14 18:15:29 +0300

    Create FindEventLog.ps1

---
+++ FindEventLog.ps1
@@ -0,0 +1,19 @@
+﻿#https://eventlogxp.com/blog/advanced-filtering-how-to-filter-events-by-event-descriptions/
+$XMLQuery = @"
+<QueryList>
+  <Query Id="0" Path="Security">
+    <Select Path="Security">*[EventData[Data[@Name='SubjectUserName'] and (Data='JohnSmith')]]</Select>
+  </Query>
+</QueryList>
+"@
+
+
+"SRV1,SRV2,SRV3".Split(",").Split(",") | % {
+    Get-WinEvent -ComputerName $_ -FilterXml $XMLQuery -ea 0 | % {
+        If ($_.KeywordsDisplayNames -eq "Audit Failure") {
+            Write-Host $_.KeywordsDisplayNames -ForegroundColor Yellow
+            $_.TimeCreated
+            $_.Message
+        }
+    }
+}
